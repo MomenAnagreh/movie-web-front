cb6643e9f26d7bd504c2d035f2579ffcfa1e78f2
Momen Anagreh <momenanagreh@momens-mbp.myfiosgateway.com>
2022-10-17 01:48:49 -0400

    Home Page Setup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { GetStartedComponent } from './home-page/get-started/get-started.component';
+import { DisplayComponent } from './home-page/display/display.component';
+import { HeaderComponent } from './home-page/header/header.component';
+import { FrequentlyAskedQuestionsComponent } from './home-page/frequently-asked-questions/frequently-asked-questions.component';
+import { SigninFormComponent } from './home-page/signin-form/signin-form.component';
+import { FooterComponent } from './home-page/footer/footer.component';
+import { LangSelectComponent } from './home-page/lang-select/lang-select.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    GetStartedComponent,
+    DisplayComponent,
+    HeaderComponent,
+    FrequentlyAskedQuestionsComponent,
+    SigninFormComponent,
+    FooterComponent,
+    LangSelectComponent
   ],
   imports: [
     BrowserModule,
