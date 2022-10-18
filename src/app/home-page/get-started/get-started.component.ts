67a6f7ffc2e9bdfabade7860a220a71bce4b37cc
Momen Anagreh <momenanagreh@momens-mbp.myfiosgateway.com>
2022-10-17 21:06:10 -0400

    Style-fixing-1

--- src/app/home-page/get-started/get-started.component.ts
+++ src/app/home-page/get-started/get-started.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-get-started',
   templateUrl: './get-started.component.html',
-  styleUrls: ['./get-started.component.css']
+  styleUrls: ['./get-started.component.css'],
 })
 export class GetStartedComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
